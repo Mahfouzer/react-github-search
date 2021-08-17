82e8d7bccf639e65ff69a10337ee24fd73e7b9f6
Mahfouzer <mahfouzs96@gmail.com>
2021-08-17 05:32:39 +0200

    Add routing to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import ListingRepos from './pages/listing-repos-page';
+import SearchingRepos from './pages/searching-repos-page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <SearchingRepos />
+          </Route>
+          <Route path="/search/:name" exact>
+            <ListingRepos />
+          </Route>
+          <Route path="*"><h1>404</h1></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
