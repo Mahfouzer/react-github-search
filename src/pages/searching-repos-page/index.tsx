2abbc44a610fa6c149abdad16b228dcb41d111b4
Mahfouzer <mahfouzs96@gmail.com>
2021-08-17 08:59:53 +0200

    create the listing page UI

--- src/pages/searching-repos-page/index.tsx
+++ src/pages/searching-repos-page/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import SearchInput from '../../components/SearchInput'
+import H1 from '../../components/styled/H1'
+import Header from '../../components/styled/Header'
+import HorizontalCenteredContainer from '../../components/styled/HorizontalCenteredContainer'
+import Img from '../../components/styled/Img'
+
+import logo from '../../logo.svg'
 
 export default function SearchingRepos() {
+
     return (
         <div>
-            <h1>SearchingRepos</h1>
+
+            <Header>
+                <H1>Github Search App</H1>
+            </Header>
+
+            <HorizontalCenteredContainer>
+                <Img src={logo} alt="github logo" imageWidth={350} imageHeight={350} />
+                <SearchInput />
+            </HorizontalCenteredContainer>
         </div>
     )
 }
