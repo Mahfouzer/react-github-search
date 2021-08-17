99dd1a61313dfc15963ce2307dcf1a3bff5fb842
Mahfouzer <mahfouzs96@gmail.com>
2021-08-17 05:31:42 +0200

    create main app pages

---
+++ src/pages/listing-repos-page/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListingRepos() {
+    return (
+        <div>
+            <h1>ListingRepos</h1>
+        </div>
+    )
+}
---
+++ src/pages/searching-repos-page/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchingRepos() {
+    return (
+        <div>
+            <h1>SearchingRepos</h1>
+        </div>
+    )
+}
