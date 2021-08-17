878107fbf9b2566b2aca790801a19632b87cd691
Mahfouzer <mahfouzs96@gmail.com>
2021-08-17 08:53:18 +0200

    create the main page

--- src/pages/listing-repos-page/index.tsx
+++ src/pages/listing-repos-page/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import SearchInput from '../../components/SearchInput';
+import H1 from '../../components/styled/H1';
+import Header from '../../components/styled/Header'
+import history from '../../history'
 
 export default function ListingRepos() {
+
+
+    const handleNewSearch = useCallback(
+
+        (keyword) => {
+            if (keyword) {
+                history.push(`/search/${keyword}`)
+            } else {
+                history.push(`/`);
+            }
+        },
+        [],
+    )
+
     return (
         <div>
-            <h1>ListingRepos</h1>
+            <Header>
+                <H1>Github Search App</H1>
+                <SearchInput inputChangeHandler={handleNewSearch} />
+            </Header>
         </div>
     )
 }
